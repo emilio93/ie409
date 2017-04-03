9d425726cc3e11f8bd1d7f95bc58e159c504ef61
Emilio Rojas <emilio93@gmail.com>
2017-04-03 04:34:15 -0600

    tarea2 advances

---
+++ tarea2/build.sh
@@ -0,0 +1,4 @@
+mkdir -p ../build
+mkdir -p ../build/tarea2
+pdflatex --output-directory=../build/tarea2 tarea2.tex
+pdflatex --output-directory=../build/tarea2 tarea2.tex
---
+++ tarea2/parte1.2/build.sh
@@ -0,0 +1,5 @@
+cd ..
+mkdir -p ../build
+mkdir -p ../build/tarea2
+pdflatex --output-directory=../build/tarea2 tarea2.tex
+pdflatex --output-directory=../build/tarea2 tarea2.tex
