13458c904a4f2d5669bccf6a5ea86f640a781b7c
Emilio Rojas <emilio93@gmail.com>
2017-04-23 04:48:06 -0600

    update matlab script
    does everything it should, needs comments

--- laboratorio1/matlab1/script.m
+++ laboratorio1/matlab1/script.m
@@ -10,7 +10,9 @@ figure
 
     % Se crea el sistema G a partir de las matrices A, B, C, D
     G = ss(A, B, C, D);
-
+    
+    % Se obtiene el sistema normalizado(ganancia=1)
+    G_n = zpk(z, p, 1);
 
 % b)
     % Se obtienen los polos y los ceros del sistema
@@ -22,37 +24,55 @@ figure
     p
     'Los ceros son:'
     z
-    
+
     % Se muestran polos y ceros en grafica
     subplot(2,2,1)
     pzmap(G)
+    title('Polos y ceros del sistema')
+    xlabel('Eje Real')
+    ylabel('Eje Imaginario')
 
 % c)
     % Se dan valores iniciales al sistema
     x_t0 = [0;-1;1];
     subplot(2,2,2)
-    plot(initial(G, x_t0))
+    
+    % Con los valores iniciales se grafica de 0 a 10 segundos
+    initial(G, x_t0)
     xlim([0,10])
+    title('Respuesta para condiciones iniciales [0, -1, 1]^T')
+    ylabel('Amplitud')
+    xlabel('Tiempo')
 
 % d)
+    % Grafica la respuesta al impulso
     subplot(2,2,3)
     impulse(G)
 
 % e)
+    % Grafica la respuesta al escalon
     subplot(2,2,4)
     step(G)
-    
+
+figure
 % f)
-    ts = 0.005;
-    t = [0:ts:10];
-    Phi = expm(A * t)
-    S_10 = Phi*x_t0
-    lsim(G,[0, 0],10)
-    
+    % i
+        t = 10;
+        Phi = expm(A * t)
+        s_10 = Phi * x_t0
+    % ii
+        % El valor del primer elemento en s_10, -0.2545 coincide con 
+        % el que se ve en la grafica para la respuesta con las condiciones
+        % iniciales de c) (x_t0).
+    % iii
+        tt = [0:0.005:20];
+        u = zeros(size(tt));
+        lsim(G, u, tt, [0; -1; 1])
+
 
 % g)
     % Se obtiene G como funcion de transferencia H
-    H = tf(ss);
+    H = tf(G)
+    
 
-% Se obtiene el sistema normalizado(ganancia=1)
-G_n = zpk(z, p, 1);
+
